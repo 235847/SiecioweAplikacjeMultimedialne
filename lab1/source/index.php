ba9811db6208ff07dca112092bdc72f87344bc66
235847 <235847@edu.p.lodz.pl>
2023-03-27 11:21:00 +0200

    lab1-odrobka

--- lab1/source/index.php
+++ lab1/source/index.php
@@ -1,1 +1,30 @@
-<?php echo "Hello World!\n"; ?>
+<?php
+$videoFile = isset($_GET['videoFile']) ? $_GET['videoFile'] : null;
+$audioFile = isset($_GET['audioFile']) ? $_GET['audioFile'] : null;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World Player</title>
+</head>
+<body>
+
+<?php if ($videoFile): ?>
+    <video id="videoPlayer" controls>
+        <source src="<?php echo htmlspecialchars($videoFile); ?>" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+<?php endif; ?>
+
+<?php if ($audioFile): ?>
+    <audio id="audioPlayer" controls>
+        <source src="<?php echo htmlspecialchars($audioFile); ?>" type="audio/mpeg">
+        Your browser does not support the audio tag.
+    </audio>
+<?php endif; ?>
+
+</body>
+</html>
