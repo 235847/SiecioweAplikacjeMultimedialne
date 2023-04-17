06b1ac92b54dd334918356e7136a1f049cfd3a32
235847 <235847@edu.p.lodz.pl>
2023-04-17 14:56:26 +0200

    Adding buttons to the players

--- lab3/source/index.php
+++ lab3/source/index.php
@@ -18,18 +18,21 @@ $imgFile = isset($_GET['imgFile']) ? $_GET['imgFile'] : null;
     <video id="videoPlayer" controls>
         <source id="videoPlayerSource" src="<?php echo htmlspecialchars($videoFile); ?>" type="video/mp4">
     </video><br/><br/>
-    <button id="videoCancel" onclick="cancelVideo()">Anuluj odtwarzanie wideo</button><br/><br/>
+    <button id="videoCancel" onclick="cancelVideo()">Anuluj odtwarzanie wideo</button>
+    <button id="videoAdd" onclick="addMedia('videoPlayer', 'Video')">Add video</button><br/><br/>
 <?php endif; ?>
 
 <?php if ($audioFile): ?>
     <audio id="audioPlayer" controls>
         <source id="audioPlayerSource" src="<?php echo htmlspecialchars($audioFile); ?>" type="audio/mpeg">
     </audio><br/><br/>
-    <button id="audioCancel" onclick="cancelAudio()">Anuluj odtwarzanie dźwięku</button><br/><br/>
+    <button id="audioCancel" onclick="cancelAudio()">Anuluj odtwarzanie dźwięku</button>
+    <button id="audioAdd" onclick="addMedia('audioPlayer', 'Audio')">Add video</button><br/><br/>
 <?php endif; ?>
 
 <?php if ($imgFile): ?>
     <img id="posterImage" src="<?php echo htmlspecialchars($imgFile); ?>" alt="Obraz">
+    <button id="imgAdd" onclick="addMedia('posterImage', 'Image')">Add image</button><br/><br/>
 <?php endif; ?>
 
 <table id="playlist_table">
